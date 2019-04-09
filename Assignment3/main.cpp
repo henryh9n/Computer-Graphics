4c62fa4b5efc111baa43ee209511191757a3b7c7
hharutyunyan <henryharutyunyan@gmail.com>
2019-04-10 01:38:39 +0400

    Initial Commit

---
+++ Assignment3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment3
+//
+//  Created by Henry Harutyunyan on 4/10/19.
+//  Copyright © 2019 hharutyunyan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
